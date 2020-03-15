81d137f9f9ccddab9751c72e5f334a053a47f698
Matt Landers <matt@platypi.io>
2020-03-15 16:08:38 -0500

    project structure

---
+++ website/server.ts
@@ -0,0 +1,12 @@
+import { serve } from "https://deno.land/std/http/server.ts";
+
+const env = Deno.env();
+
+const port: number = parseInt(env?.PORT ?? "3000");
+
+const s = serve({ port });
+console.log(`http://localhost:/${port}`);
+
+for await (const req of s) {
+  req.respond("OK");
+}
